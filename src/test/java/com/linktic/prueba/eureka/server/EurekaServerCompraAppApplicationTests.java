27a33cefd03f3804aa0f3057d0c1b354dfb42123
Harold Negrete <negreteharold@gmail.com>
2025-07-10 22:47:10 -0500

    Servidor Eureka

---
+++ src/test/java/com/linktic/prueba/eureka/server/EurekaServerCompraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktic.prueba.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerCompraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
